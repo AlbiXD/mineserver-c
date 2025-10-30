e355a79479869e2667a89ad0a0de6851e1ee1bc7
Albi Zhaku <albizhaku15@gmail.com>
2025-10-30 17:59:21 -0400

    Added get_line for config parsing

--- include/util.h
+++ include/util.h
@@ -2,6 +2,16 @@
 #define UTIL_H
 
 #include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * Reads one line from a file descriptor.
+ *
+ * @param fd       File descriptor to read from.
+ * @param buf      Buffer to store the line.
+ * @param max_len  Max number of bytes to read.
+ * @return Number of bytes read (0 on EOF).
+ */
 int get_line(int fd, char* buf, size_t maxlen);
 
 
--- src/util.c
+++ src/util.c
@@ -1,13 +1,5 @@
 #include "../include/util.h"
 
-/**
- * Reads one line from a file descriptor.
- *
- * @param fd       File descriptor to read from.
- * @param buf      Buffer to store the line.
- * @param max_len  Max number of bytes to read.
- * @return Number of bytes read (0 on EOF).
- */
 int get_line(int fd, char *buf, size_t max_len)
 {
     char c;
